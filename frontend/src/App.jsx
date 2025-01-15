71b2cb82a24009f2de25ed48df7e640c00bc5fe9
leandroatochkin <leandro.atochkin@outlook.es>
2025-01-15 18:07:07 -0300

    ui update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,86 +1,140 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Canvas from "./components/Canvas/Canvas";
 import Asset from "./components/Asset/Asset";
-import { vehicles, traffic, people, scenarios } from "./utils/data";
+import {
+  animals,
+  arrows,
+  bikes,
+  bridges,
+  busses,
+  cars,
+  events,
+  pedestrians,
+  streets,
+  trafficlights,
+  trucks,
+} from "./utils/data";
 
-
+// Mapping internal keys to human-readable labels
+const categories = {
+  animals: { name: "Animales", data: animals },
+  arrows: { name: "Flechas", data: arrows },
+  bikes: { name: "Bicicletas & Motos", data: bikes },
+  bridges: { name: "Puentes", data: bridges },
+  busses: { name: "Colectivos", data: busses },
+  cars: { name: "Autos", data: cars },
+  events: { name: "Eventos", data: events },
+  pedestrians: { name: "Peatones", data: pedestrians },
+  streets: { name: "Calles & Rutas", data: streets },
+  trafficlights: { name: "Semáforos", data: trafficlights },
+  trucks: { name: "Camiones", data: trucks },
+};
 
 const App = () => {
   const [selectedAsset, setSelectedAsset] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(vehicles);
-
+  const [selectedCategory, setSelectedCategory] = useState("cars");
 
   const handleRemoveAsset = (index) => {
     setSelectedAsset(selectedAsset.filter((asset, i) => i !== index));
-  }
+  };
 
+  useEffect(() => {
+    console.log(selectedCategory);
+  }, [selectedCategory]);
 
   return (
-    <div style={{display: 'flex', width: '100vw', height: '100vh', overflow: 'hidden'}}>
-      <div style={{width: '70%', height: '100vh'}}>
-      <Canvas>
-        {selectedAsset && selectedAsset.map((src, index) => (
-          <Asset key={index} src={src} handleRemoveAsset={()=>handleRemoveAsset(index)}/>
-        ))}
-      </Canvas>
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <div style={{ width: "70%", height: "100vh" }}>
+        <Canvas>
+          {selectedAsset &&
+            selectedAsset.map((src, index) => (
+              <Asset
+                key={index}
+                src={src}
+                handleRemoveAsset={() => handleRemoveAsset(index)}
+              />
+            ))}
+        </Canvas>
       </div>
-      {/*-------------------vehicles--------------------*/}
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        overflow: 'scroll',
-        scrollbarWidth: 'none',
-        width: '25%',
-      }}>
-      <div>
-        <button onClick={()=>setSelectedCategory(vehicles)}>Vehículos</button>
-        <button onClick={()=>setSelectedCategory(traffic)}>Tráfico</button>
-        <button onClick={()=>setSelectedCategory(people)}>Personas</button>
-        <button onClick={()=>setSelectedCategory(scenarios)}>Escenarios</button>
-      </div>
-      <div style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, 1fr)',
-        gridGap: '10px',
-        height: '100%', 
-        width: '20%',
-        
-        }}>
-        
-        {selectedCategory.map((src, index) => (
-          <div key={index}
-            onClick={() => setSelectedAsset((prev)=>[...prev, src])} 
-            style={{
-            backgroundImage: `url(${src})`,
-            backgroundSize: 'cover',
-            width: '200px',
-            height: '150px',
-            cursor: 'pointer',
-          }}>
-          </div>
-        ))}
-      </div>
-      </div>
-      <div style={{
-        width: '10%',
-        height: '100vh',
-      }}>
-        <div style={{
-          top: '0',
-          height: '70%',
-          clipPath: 'polygon(0 0, 100% 0, 100% 90%, 0 100%)',
-          backgroundColor: 'red'
-        }}>
-        <h1 style={{
-          transform: 'rotate(90deg)',
-          width: '100%',
-          whiteSpace: 'nowrap',
-          paddingTop: '60%',
-          paddingLeft: '70%'
-        }}>MOTION CRUSH 1.0</h1>
-        </div>
 
+      {/* Sidebar */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          overflow: "scroll",
+          scrollbarWidth: "none",
+          width: "25%",
+        }}
+      >
+        {/* Category Selector */}
+        <select
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          value={selectedCategory}
+        >
+          {Object.entries(categories).map(([key, { name }]) => (
+            <option key={key} value={key}>
+              {name}
+            </option>
+          ))}
+        </select>
+
+        {/* Assets Display */}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gridGap: "10px",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          {categories[selectedCategory].data.map((src, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectedAsset((prev) => [...prev, src])}
+              style={{
+                backgroundImage: `url(${src})`,
+                backgroundSize: "cover",
+                width: "200px",
+                height: "150px",
+                cursor: "pointer",
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+
+      {/* Right Side (Diagonal Bar) */}
+      <div style={{ width: "10%", height: "100vh" }}>
+        <div
+          style={{
+            top: "0",
+            height: "70%",
+            clipPath: "polygon(0 0, 100% 0, 100% 90%, 0 100%)",
+            backgroundColor: "red",
+          }}
+        >
+          <h1
+            style={{
+              transform: "rotate(90deg)",
+              width: "100%",
+              whiteSpace: "nowrap",
+              paddingTop: "60%",
+              paddingLeft: "70%",
+            }}
+          >
+            MOTION CRUSH 1.0
+          </h1>
+        </div>
       </div>
     </div>
   );
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,39 +1,118 @@
-export const vehicles = [
-        "/public/vehicles/bicycle1.png",
-        "/public/vehicles/bicycle2.png",
-        "/public/vehicles/bike1.png",
-        "/public/vehicles/bus1.png",
-        "/public/vehicles/bus2.png",
-        "/public/vehicles/bus3.png",
-        "/public/vehicles/car1.png",
-        "/public/vehicles/car2.png",
-        "/public/vehicles/car3.png",
-        "/public/vehicles/car4.png",
-        "/public/vehicles/truck1.png",
-        "/public/vehicles/truck2.png",
-        "/public/vehicles/truck3.png",
-        "/public/vehicles/truck4.png",
+export const animals =[
+    '/public/animals/burro1.png',
+    '/public/animals/burro2.png',
+    '/public/animals/caballo1.png',
+    '/public/animals/caballo2.png',
+    '/public/animals/caballo3.png',
+    '/public/animals/carpincho1.png',
+    '/public/animals/carpincho2.png',
+    '/public/animals/perro1.png',
+    '/public/animals/perro2.png',
+    '/public/animals/perro3.png',
+    '/public/animals/perro4.png',
+    '/public/animals/vaca1.png',
+    '/public/animals/vaca2.png',
+    '/public/animals/yacare1.png',
+    '/public/animals/yacare2.png',
 ]
 
-export const scenarios = [
-    "/public/scenarios/bridge1.png",
-    "/public/scenarios/bridge2.png",
-    "/public/scenarios/bridge3.png",
-    "/public/scenarios/bridge4.png",
-    "/public/scenarios/street1.png",
-    "/public/scenarios/street2.png",
-    "/public/scenarios/street3.png",
-    "/public/scenarios/street4.png",
-    "/public/scenarios/street5.png",
+export const arrows = [
+    '/public/arrows/flecha1.png',
+    '/public/arrows/flecha2.png',
+    '/public/arrows/flecha3.png',
+    '/public/arrows/flecha4.png',
+    '/public/arrows/flecha5.png',
+    '/public/arrows/flecha6.png',
+    '/public/arrows/flecha7.png',
+    '/public/arrows/flecha8.png',
+    '/public/arrows/flecha9.png',
 ]
 
-export const traffic = [
-    "/public/traffic/tollbooth1.png",
-    "/public/traffic/tollbooth2.png",
-    "/public/traffic/trafficlight.png",
+export const bikes = [
+    '/public/bikes/bicycle1.png',
+    '/public/bikes/bicycle2.png',
+    '/public/bikes/bike1.png',
+    '/public/bikes/bike2.png',
+    '/public/bikes/bike3.png'
 ]
 
-export const people = [
-    "/public/people/person1.png",
-    "/public/people/person2.png",
+export const bridges = [
+    '/public/bridges/bridge1.png',
+    '/public/bridges/bridge2.png',
+    '/public/bridges/bridge3.png',
 ]
+
+export const busses = [
+    '/public/busses/bus1.png',
+    '/public/busses/bus2.png',
+    '/public/busses/bus3.png',
+    '/public/busses/bus4.png',
+    '/public/busses/bus5.png',
+    '/public/busses/bus6.png',
+]
+
+export const cars = [
+    '/public/cars/car1.png',
+    '/public/cars/car2.png',
+    '/public/cars/car3.png',
+    '/public/cars/car4.png',
+    '/public/cars/car5.png',
+    '/public/cars/car6.png',
+    '/public/cars/car7.png',
+    '/public/cars/car8.png',
+    '/public/cars/car9.png',
+    '/public/cars/car10.png',
+    '/public/cars/car11.png'
+    ]
+
+    export const events = [
+        '/public/events/event1.png',
+        '/public/events/event2.png',
+        '/public/events/event3.png',
+        '/public/events/event4.png',
+        '/public/events/event5.png',
+        '/public/events/event6.png',
+        '/public/events/event7.png',
+        '/public/events/event8.png',
+        '/public/events/event9.png',
+        '/public/events/event10.png',
+        '/public/events/event11.png',
+        '/public/events/event12.png',
+        '/public/events/event13.png',
+    ]
+
+    export const pedestrians = [
+        '/public/pedestrians/pedestrian1.png',
+        '/public/pedestrians/pedestrian2.png',
+    ]
+
+    export const streets = [
+        '/public/streets/street1.png',
+        '/public/streets/street2.png',
+        '/public/streets/street3.png',
+        '/public/streets/street4.png',
+        '/public/streets/street5.png',
+        '/public/streets/street6.png',
+        '/public/streets/street7.png',
+        '/public/streets/street8.png',
+        '/public/streets/street9.png',
+    ]
+
+    export const trafficlights = [
+        '/public/trafficlights/trafficlight1.png',
+        '/public/trafficlights/trafficlight2.png',
+        '/public/trafficlights/trafficlight3.png',
+        '/public/trafficlights/trafficlight4.png',
+    ]
+
+    export const trucks =[
+        '/public/trucks/truck1.png',
+        '/public/trucks/truck2.png',
+        '/public/trucks/truck3.png',
+        '/public/trucks/truck4.png',
+        '/public/trucks/truck5.png',
+        '/public/trucks/truck6.png',
+        '/public/trucks/truck7.png',
+        '/public/trucks/truck8.png',
+        '/public/trucks/truck9.png',
+    ]
